310a5425cd6d2e39ee377ffabcaf8258b553a033
Zeki <yaren.zack@gmail.com>
2019-01-19 16:52:13 -0500

    small changes

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,12 +1,15 @@
 public class Calculator {
 
+    //testtt
+
     public static double add(double num1,double num2){
         double result = num1+num2;
         return result;
 
     }
 
-    public static void double substract (double num1,double num2){
+    public static  double substract (double num1,double num2){
+
         return num1-num2;
     }
 
