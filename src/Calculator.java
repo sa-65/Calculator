7dbf7c98eb05d9e9625bc294f8441858cdd96d74
yaren65 <46764145+yaren65@users.noreply.github.com>
2019-01-19 16:37:58 -0500

    Multiply method added

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,9 +6,15 @@ public class Calculator {
 
     }
 
-    public static void double substract (double num1,double num2){
+    public static  double substract (double num1,double num2){
         return num1-num2;
     }
+    
+    public static  double multiply (double num1,double num2){
+        return num1*num2;
+    }
+    
+    
 
 
 }
